7a6a37ba259cad7a751dc5c71808cb5bd974ff36
Thanapat Nawang <thanapattt.m@gmail.com>
2024-10-06 19:42:57 +0700

    footer

---
+++ src/components/footer.php
@@ -0,0 +1,8 @@
+<aside class="text-sm">
+    <p>© <?= date("Y"); ?> - Pandora Mini Project. From a com-sci students.</p>
+</aside>
+<nav class="text-lg hover:text-primary transition duration-300 ease-in-out">
+    <a href="https://github.com/FeiZGe/PHP-Movie-MiniProject" data-tip="Repository" class="tooltip sm:tooltip-left tooltip-primary">
+        <i class="fa-brands fa-github"></i>
+    </a>
+</nav>
