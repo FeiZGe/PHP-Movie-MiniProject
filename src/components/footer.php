c768c3e1cc75c3cb6035147a9835f1d673de7973
Thanapat Nawang <thanapattt.m@gmail.com>
2024-10-06 19:43:52 +0700

    add blank link

--- src/components/footer.php
+++ src/components/footer.php
@@ -2,7 +2,7 @@
     <p>© <?= date("Y"); ?> - Pandora Mini Project. From a com-sci students.</p>
 </aside>
 <nav class="text-lg hover:text-primary transition duration-300 ease-in-out">
-    <a href="https://github.com/FeiZGe/PHP-Movie-MiniProject" data-tip="Repository" class="tooltip sm:tooltip-left tooltip-primary">
+    <a href="https://github.com/FeiZGe/PHP-Movie-MiniProject" target="_blank" data-tip="Repository" class="tooltip sm:tooltip-left tooltip-primary">
         <i class="fa-brands fa-github"></i>
     </a>
 </nav>
