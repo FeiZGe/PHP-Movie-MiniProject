6e0b843a8f9a6d9d6446701e5fcb0bbfc19c45ac
Poonnys <poonyapat.wongsatit@gmail.com>
2024-10-06 19:47:01 +0700

    prepare movie
    prepare movie for data

--- src/movie.php
+++ src/movie.php
@@ -15,30 +15,83 @@
 
 </head>
 
-<body class="font-poppins">
+<body class="font-poppins overflow-hidden">
     <header>
         <?php include('./components/nav.php'); ?>
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
+
     </header>
 
-    <!-- Start Main Content -->
-    <main class="container mx-auto px-4 mt-10">
+     <!-- Start Main Content -->
+     <main class="container mx-auto mt-0 ">
         <!-- ภาพหนัง -->
-        <div class="relative">
-            <div class="bg-cover bg-center w-full h-64 mb-4" style="background-image: url('บลาๆๆ.jpg');">
-                <!-- ปุ่มเล่น -->
-                <div class="absolute bottom-16 right-1 rounded-full p-3 shadow-lg btn btn-primary text-white"> Play </div>
+        <div class="relative bg-cover bg-center w-full h-screen mb-4 bg-black" style="width: 100vw; margin-left: calc(-50vw + 50%);">
+
+            <!-- Container สำหรับชื่อและปุ่มเล่น -->
+            <section class="absolute left-0 flex justify-between w-full top-60 px-40">
+            <!-- ชื่อและคำอธิบายหนัง -->
+            <div>
+                <h2 class=" uppercase text-6xl font-bold ">Movie Name</h2>
+                <h3 class="max-w-2xl text-l font-normal mt-4 whitespace-normal opacity-60">Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas natus maxime qui voluptatem magni inventore, molestiae est dolor illum eligendi repellendus pariatur dicta nulla delectus dolorum aliquam, maiores omnis quidem?</h3>
             </div>
 
-            <!-- ชื่อ แนะนำ-->
-            <section class="mt-20">
-                <h2 class="max-w-5xl text-4xl font-bold mr-4">Movie Name</h2>
-                <h3 class="max-w-5xl text-xl font-normal mt-4">Description</h3>
-            </section>
-        </div>
+            <!-- ปุ่มเล่น -->
+                <div class=" btn btn-primary hover:scale-110 ease-in-out rounded-full shadow-lg transform transition-transform duration-300 hover:z-50">
+                <i class="fas fa-play"></i>
+                </div>
+                </section>
+         
+        </div>  
+
+        <section class="absolute left-0 right-0 z-20 mt-[-400px] mb-4 px-8">
+            <div>
+                <h3 class="text-xl font-normal mb-2 pl-52">Also Be Like</h3>
+            </div> 
+            <!-- Movie rec -->
+            <article class=" h-52 flex flex-row justify-center items-center gap-3 z-10 ">
+
+                <!-- 1 -->
+                <div class="card bg-base-300 w-60 h-48 shadow-xl text-center relative transform transition-transform duration-300 hover:scale-125 hover:z-50">
+                    <!-- pic movie -->
+                    <div class="m-4 h-3/5 rounded-lg"></div>
+                    </div>
+
+                <!-- 2 -->
+                <div class="card bg-base-300 w-60 h-48 shadow-xl text-center relative transform transition-transform duration-300 hover:scale-125 hover:z-50">
+                    <!-- pic movie -->
+                    <div class="m-4 h-3/5 rounded-lg"></div>
+                    </div>
+
+                 <!-- 3 -->
+                <div class="card bg-base-300 w-60 h-48 shadow-xl text-center relative transform transition-transform duration-300 hover:scale-125 hover:z-50">
+                    <!-- pic movie -->
+                    <div class="m-4 h-3/5 rounded-lg"></div>
+                    </div>
+
+                <!-- 4 -->
+                <div class="card bg-base-300 w-60 h-48 shadow-xl text-center relative transform transition-transform duration-300 hover:scale-125 hover:z-50">
+                    <!-- pic movie -->
+                    <div class="m-4 h-3/5 rounded-lg"></div>
+                    </div>
+
+                <!-- 5 -->
+                <div class="card bg-base-300 w-60 h-48 shadow-xl text-center relative transform transition-transform duration-300 hover:scale-125 hover:z-50">
+                    <!-- pic movie -->
+                    <div class="m-4 h-3/5 rounded-lg"></div>
+                    </div>
+
+                <!-- 6 -->
+                <div class="card bg-base-300 w-60 h-48 shadow-xl text-center relative transform transition-transform duration-300 hover:scale-125 hover:z-50">
+                    <!-- pic movie -->
+                    <div class="m-4 h-3/5 rounded-lg"></div>
+                    </div>
+
+            </article>
+        </section>
+
     </main>
     <!-- End Main Content -->
 
     <footer> </footer>
 </body>
 </html>
-ื
