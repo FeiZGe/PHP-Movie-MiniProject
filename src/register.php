7ea274c104bbb9d04d142fe851b9318d65503231
Thanapat Nawang <thanapattt.m@gmail.com>
2024-10-12 16:33:20 +0700

    Update form
    create username email password

--- src/register.php
+++ src/register.php
@@ -22,8 +22,85 @@
     </header>
 
     <!-- Start main content -->
-    <main>
+    <main class="container mx-auto px-3 mt-3">
+        <div class="grid grid-cols-1 h-screen items-center">
+            <div class="card bg-base-100 shadow-xl w-full md:w-2/5 mx-auto">
+                <div class="card-body">
+                    <h1 class="font-raleway text-lg font-bold">Pandora</h1>
+                    <div class="flex flex-col items-center">
 
+                        <div>
+                            <!-- titile -->
+                            <div class="mt-3 flex flex-col gap-1 items-center text-center">
+                                <h2 class="card-title text-3xl text-wrap">Register an Account</h2>
+                                <p class="text-sm text-wrap">Enter your personal data to create your account.</p>
+                            </div>
+
+                            <div class="divider divider-neutral"></div>
+
+                            <form action="#" class="mt-5">
+
+                                <!-- input -->
+                                <div class="flex flex-col gap-3">
+                                    <!-- Username -->
+                                    <label for="username" class="input input-md input-bordered flex items-center gap-2 hover:input-primary invalid:input-error">
+                                        <svg
+                                            xmlns="http://www.w3.org/2000/svg"
+                                            viewBox="0 0 16 16"
+                                            fill="currentColor"
+                                            class="h-4 w-4 opacity-70">
+                                            <path
+                                            d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6ZM12.735 14c.618 0 1.093-.561.872-1.139a6.002 6.002 0 0 0-11.215 0c-.22.578.254 1.139.872 1.139h9.47Z" />
+                                        </svg>
+                                        <input type="text" name="username" class="grow" placeholder="Username" />
+                                    </label>
+
+                                    <!-- email -->
+                                    <label for="email" class="input input-md input-bordered flex items-center gap-2 hover:input-primary invalid:input-error">
+                                        <svg
+                                            xmlns="http://www.w3.org/2000/svg"
+                                            viewBox="0 0 16 16"
+                                            fill="currentColor"
+                                            class="h-4 w-4 opacity-70">
+                                            <path
+                                            d="M2.5 3A1.5 1.5 0 0 0 1 4.5v.793c.026.009.051.02.076.032L7.674 8.51c.206.1.446.1.652 0l6.598-3.185A.755.755 0 0 1 15 5.293V4.5A1.5 1.5 0 0 0 13.5 3h-11Z" />
+                                            <path
+                                            d="M15 6.954 8.978 9.86a2.25 2.25 0 0 1-1.956 0L1 6.954V11.5A1.5 1.5 0 0 0 2.5 13h11a1.5 1.5 0 0 0 1.5-1.5V6.954Z" />
+                                        </svg>
+                                        <input type="email" name="email" class="grow" placeholder="Email" />
+                                    </label>
+
+                                    <!-- Password -->
+                                    <label for="password" class="input input-md input-bordered flex items-center gap-2 hover:input-primary invalid:input-error">
+                                        <svg
+                                            xmlns="http://www.w3.org/2000/svg"
+                                            viewBox="0 0 16 16"
+                                            fill="currentColor"
+                                            class="h-4 w-4 opacity-70">
+                                            <path
+                                            fill-rule="evenodd"
+                                            d="M14 6a4 4 0 0 1-4.899 3.899l-1.955 1.955a.5.5 0 0 1-.353.146H5v1.5a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1-.5-.5v-2.293a.5.5 0 0 1 .146-.353l3.955-3.955A4 4 0 1 1 14 6Zm-4-2a.75.75 0 0 0 0 1.5.5.5 0 0 1 .5.5.75.75 0 0 0 1.5 0 2 2 0 0 0-2-2Z"
+                                            clip-rule="evenodd" />
+                                        </svg>
+                                        <input type="password" name="password" class="grow" placeholder="Password" />
+                                    </label>
+                                </div>
+
+                                <!-- Submit -->
+                                <div class="flex items-center justify-center">
+                                    <input type="submit" value="Confirm" class="btn btn-primary btn-wide mt-7 hover:scale-110 transition duration-300 ease-in-out">
+                                </div>
+
+                                <!-- more detail -->
+                                <p class="text-xs opacity-75 mt-2 flex justify-center gap-1">
+                                    Do you have an account? <a href="login.php" class="link link-secondary">Sign In</a>
+                                </p>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     </main>
     <!-- End main content -->
 
