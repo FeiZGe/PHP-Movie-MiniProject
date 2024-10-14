c491b9ff4ebbd5f22520dcd02557ac52712872c5
Thanapat Nawang <thanapattt.m@gmail.com>
2024-10-14 16:07:59 +0700

    design admin page

---
+++ src/admin/adminnav.php
@@ -0,0 +1,54 @@
+<nav class="fixed z-30 top-1 left-1/2 transform -translate-x-1/2">
+    <div class="flex flex-row items-center gap-4 rounded-full bg-neutral text-neutral-content w-56 px-2 py-1.5 mt-6 drop-shadow-[0_5px_8px_rgba(52,145,252,0.25)]">
+        <!-- home -->
+        <div class="flex-1 justify-start">
+            <a href="/dashboard.php" class="px-3 py-1 rounded-full hover:bg-primary hover:text-primary-content transition duration-300 ease-in-out">Home</a>
+        </div>
+        
+        <!-- rank -->
+        <div class="flex flex-1 justify-end">
+            <a href="#" class="flex flex-row items-center gap-2 px-3 py-1 rounded-full bg-primary text-primary-content hover:bg-info hover:text-info-content transition duration-300 ease-in-out">
+                <!-- icon -->
+                <i class="fa-solid fa-id-card"></i>
+
+                <!-- btn -->
+                <p>Role</p>
+            </a>
+        </div>
+    </div>
+</nav>
+
+<!-- bar -->
+<section class="flex flex-row items-center mx-4 py-1 mt-6">
+
+    <!-- web name -->
+    <article class="flex-1 flex justify-start items-center font-raleway font-bold text-2xl px-3">
+        Pandora
+    </article>
+
+    <!-- user -->
+    <article class="flex-1 flex justify-end items-center">
+        <!-- avatar -->
+        <section class="col-span-1 flex items-center justify-end gap-2">
+            <article class="avatar">
+                <div class="w-10 rounded-full">
+                    <img src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp" />
+                </div>
+            </article>
+            <div class="dropdown dropdown-end">
+                <div tabindex="0" role="button" class="btn btn-ghost btn-sm">
+                    <!-- Name -->
+                    <article class="flex flex-row gap-1">
+                        <h3>Admin</h3>
+                        <i class="fa-solid fa-angle-down"></i>
+                    </article>
+                </div>
+                <ul
+                tabindex="0"
+                class="menu menu-sm dropdown-content bg-base-300 rounded-box z-[1] mt-3 w-52 p-2 shadow">
+                    <li><a href="logout.php" class="bg-error text-error-content hover:bg-red-600 my-1">Logout</a></li>
+                </ul>
+            </div>
+        </section>
+    </article>
+</section>
--- src/admin/dashboard.php
+++ src/admin/dashboard.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="en" data-theme="winter">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -9,14 +9,103 @@
     <title>Admin | Pandora</title>
 
     <!-- Tailwind + DaisyUI -->
-    <link rel="stylesheet" href="./style/output.css">
+    <link rel="stylesheet" href="../style/output.css">
 
     <!-- LINK -->
     <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Raleway:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css">
 
 </head>
-<body>
-    <div>TEST ADMIN</div>
+<body class="font-poppins">
+    <header>
+        <?php include('adminnav.php'); ?>
+    </header>
+    
+    <!-- Start Main Content -->
+    <main class="container mx-auto px-3 mt-3">
+
+        <!-- hero -->
+        <section class="w-full h-56 bg-cover bg-no-repeat bg-center mt-5 rounded-xl drop-shadow-[0_5px_8px_rgba(52,145,252,0.25)]" style="background-image: url(adminbg.jpg)">
+
+        </section>
+
+        <!-- table -->
+        <section class="mt-4">
+
+            <!-- titile -->
+            <article class="flex flex-row items-center justify-between">
+                <h2 class="text-3xl font-bold bg-gradient-to-br from-base-content to-primary bg-clip-text text-transparent">Movie lists</h2>
+                <button class="btn btn-primary btn-sm flex flex-row justify-center items-center gap-1 transition duration-300 ease-in-out hover:scale-110">
+                    <i class="fa-solid fa-plus"></i>
+                    <p>movie</p>
+                </button>
+            </article>
+
+            <article class="card bg-base-100 shadow-xl mt-5">
+                <div class="card-body">
+                    <div class="overflow-x-auto">
+                        <table class="table">
+                            <!-- head -->
+                            <thead>
+                            <tr>
+                                <th>Name</th>
+                                <th>Story</th>
+                                <th class="text-center">Action</th>
+                            </tr>
+                            </thead>
+                            <tbody>
+                            <!-- row 1 -->
+                            <tr>
+                                <td>
+                                    <div class="flex items-center gap-3">
+                                        <div class="w-20 h-28 rounded-lg bg-cover bg-no-repeat bg-center" style="background-image: url(adminbg.jpg)">
+                                            
+                                        </div>
+                                        <div>
+                                            <!-- Movie name -->
+                                            <div class="font-bold">
+                                                Avatar
+                                            </div>
+
+                                            <!-- Movie genre -->
+                                            <div class="badge badge-primary badge-sm badge-outline">
+                                                Fantasy
+                                            </div>
+                                        </div>
+                                    </div>
+                                </td>
+                                <td>
+                                    <div class="text-pretty">
+                                        Jake Sully ทหารหนุ่มที่พิการ ถูกส่งไปยังดาว Pandora ในภารกิจสำรวจและค้นหาทรัพยากรที่สำคัญ แต่เขากลับพบว่าความเชื่อมโยงระหว่างเผ่า Na'vi และธรรมชาติทำให้เขาต้องเลือกระหว่างการปกป้อง Pandora กับการรับใช้อาณานิคมของมนุษย์
+                                    </div>
+                                </td>
+                                <td>
+                                    <div class="flex flex-row justify-center items-center gap-1.5">
+
+                                        <!-- edit btn -->
+                                        <a href="#" class="btn btn-warning btn-square btn-sm tooltip tooltip-top tooltip-warning flex items-center justify-center transition duration-300 ease-in-out hover:scale-125" data-tip="Edit">
+                                            <i class="fa-solid fa-pen-to-square"></i>
+                                        </a>
+                                        <a href="#" class="btn btn-error btn-square btn-sm tooltip tooltip-top tooltip-error flex items-center justify-center transition duration-300 ease-in-out hover:scale-125" data-tip="Delete">
+                                            <i class="fa-solid fa-trash"></i>
+                                        </a>
+                                    </div>
+                                </td>
+                            </tr>
+                            
+                            </tbody>
+                            
+                        </table>
+                    </div>
+                </div>
+            </article>
+        </section>
+    </main>
+    <!-- End Main Content -->
+
+    <footer class="text-neutral-500 items-center p-4 container mx-auto flex flex-col-reverse justify-center sm:flex-row sm:justify-between gap-1">
+        <?php include('../components/footer.php'); ?>
+    </footer>
+
 </body>
 </html>
